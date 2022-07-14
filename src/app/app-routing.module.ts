0acd6f2d8de41c4fefe1b14a2133d201aec2f1d2
Lukinh4Z <carvalholnunes@gmail.com>
2022-07-14 11:48:17 -0300

    modified:   src/app/app-routing.module.ts modified:   src/index.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // ele vai analisar todo o roteamento e ter certeza que
+  // não vai ter nada além do localhost:4200/ e assim vai fazer o
+  // redirecionamento para courses.
   { path: '', pathMatch: 'full', redirectTo: 'courses' },
   {
     path: 'courses',
