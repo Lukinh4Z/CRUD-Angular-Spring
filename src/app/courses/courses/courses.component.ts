938e6b1051f495c8797a68de5d1255670759a489
Lukinh4Z <carvalholnunes@gmail.com>
2022-07-14 12:43:53 -0300

    modified:   src/app/courses/courses/courses.component.ts modified:   src/app/courses/services/courses.service.ts new file:   src/assets/courses.json

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
@@ -9,18 +10,35 @@ import { CoursesService } from '../services/courses.service';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   // coursesService: CoursesService;
 
   constructor(private coursesService: CoursesService) {
-    // this.courses = [] //inicializando a variável dentro do construtor ao invés de na declaração, mas tanto faz.
-    // this.coursesService = new CoursesService();
-    // this.courses = this.coursesService.list();
+    /**
+     *
+     * this.courses = [] //inicializando a variável dentro do construtor ao invés de na declaração, mas tanto faz.
+     *
+     * Isso aqui é uma instância manual, mas fizemos a injeção direta dentro do construtor da classe
+     * assim não precisamos criar a variável e instanciá-la, ela vem direto,
+     * isso só é possível por causa do "@injectable"
+     * this.coursesService = new CoursesService();
+     *
+     * Poderia fazer também com o ".subscribe" para transformar o observable em array:
+     * this.courses = this.coursesService.list().subscribe(courses => this.courses = courses)
+     * mas dessa forma a declaração da variável courses deveria ser --> courses: Course[] = [];
+     * Além disso, poderia usar tbm um pipe chamado "async" para fazer o subscribe automaticamente, pra fazer iss,
+     * lá no template deveríamos definir o dataSource como: --> [dataSource]="courses | async"
+     * Acontece que nao precisamos fazer isso porque o MATERIAL TABLE sabe trabalhar automaticamente com observables!
+     * Isso poderia ser necessário se fossemos trabalhar com essa informação usando um ngFor, por exemplo.
+     *
+     */
+
+    this.courses = this.coursesService.list();
   }
 
   ngOnInit(): void {
-    this.courses = this.coursesService.list();
+    // this.courses = this.coursesService.list();
   }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,19 +1,29 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs';
 
 import { Course } from './../model/course';
 
+// esse decorator pede que o Angular forneça uma instância dessa classe automaticamente,
+// inserindo-o como dependência da classe
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
+  private readonly API = '/assets/courses.json';
   constructor(private httpClient: HttpClient) {}
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'Front-End' },
-      { _id: '2', name: 'Java', category: 'Back-End' },
-      { _id: '3', name: 'Spring-boot', category: 'Back-End' },
-    ];
+  list() {
+    /**
+     * esse operador 'diamante' <>, é para indicar que o get
+     * retorna um arrya de Course.
+     */
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      // com esse first eu estou dizendo pro servidor finalizar a inscrição do rxjs,
+      // faço isso porque não vou mais precisar de respostas do servidor;
+      // poderia usar tbm "take(1)".
+      first(),
+      tap((courses) => console.log(courses))
+    );
   }
 }
